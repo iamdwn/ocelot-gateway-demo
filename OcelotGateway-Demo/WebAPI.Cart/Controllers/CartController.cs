aadebeaf50f7aa21ae8b986bfe909cbd89da41c1
iamdwn <truongdinhdongduong@gmail.com>
2024-09-13 13:37:33 +0700

    feat: add api of cart and product

---
+++ OcelotGateway-Demo/WebAPI.Cart/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Cart.Controllers
+{
+    [Route("~/cart-api/[controller]/[action]")]
+    [ApiController]
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string First()
+        {
+            return "Cart service - First";
+        }
+
+        [HttpGet]
+        public string Second()
+        {
+            return "Cart service - Second";
+        }
+    }
+}
---
+++ OcelotGateway-Demo/WebAPI.Product/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Product.Controllers
+{
+    [Route("~/product-api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string First()
+        {
+            return "Product service - First";
+        }
+
+        [HttpGet]
+        public string Second()
+        {
+            return "Product service - Second";
+        }
+    }
+}
