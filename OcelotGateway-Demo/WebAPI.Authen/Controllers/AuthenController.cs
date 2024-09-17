aca35dfc0a85bbc4f605a7da20cbd3cb83838295
iamdwn <truongdinhdongduong@gmail.com>
2024-09-17 13:17:41 +0700

    feat: add create and get token

---
+++ OcelotGateway-Demo/WebAPI.Authen/Controllers/AuthenController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace WebAPI.Authen.Controllers
+{
+    [Route("~/authen-api/[controller]/[action]")]
+    public class AuthenController : Controller
+    {
+        private readonly ILogger<AuthenController> _logger;
+
+        public AuthenController(ILogger<AuthenController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string name, string pwd)
+        {
+            if (name == "duong" && pwd == "1")
+            {
+                var now = DateTime.UtcNow;
+
+                var claims = new Claim[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, name),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, now.ToUniversalTime().ToString(), ClaimValueTypes.Integer64),
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("dozun key"));
+                var tokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = key,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+
+                var jwt = new JwtSecurityToken(
+                    claims: claims,
+                    notBefore: now,
+                    expires: now.Add(TimeSpan.FromMinutes(2)),
+                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                    );
+
+                var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+                var responseJson = new
+                {
+                    access_token = encodedJwt,
+                    expires_in = (int)TimeSpan.FromMinutes(2).TotalSeconds
+                };
+
+                return Json(responseJson);
+            }
+            else
+            {
+                return Json("");
+            }
+        }
+    }
+}
